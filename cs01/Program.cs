2d22f947448577a4cb8f6153dd6f95998b5becbf
HuynhVanDung <vandung1307200@gmail.com>
2025-08-26 01:18:57 +0700

    [cs01] HelloWorld

---
+++ cs01/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace cs01
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Xin chao C# !");
+        }
+    }
+}
