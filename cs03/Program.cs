3a3c61fde06da88242d6714057f5a6711a1bfbc0
HuynhVanDung <vandung1307200@gmail.com>
2025-08-26 21:35:46 +0700

    [cs03] cac toan tu so hoc,toan tu tang, giam

---
+++ cs03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace cs03
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            // + - * / %
+            float a = 5;
+            int b = 4;
+
+            System.Console.WriteLine("a + b = {0}", a + b);
+            System.Console.WriteLine("a - b = {0}", a - b);
+            System.Console.WriteLine("a * b = {0}", a * b);
+            System.Console.WriteLine("a / b = {0}", a / b);
+            System.Console.WriteLine("a % b = {0}", a % b);
+
+            float kq = 6 / (2 + 3) * 4;
+            System.Console.WriteLine(kq);
+
+            // Gan 
+            // = += -= *= /= %=
+            int x = 10;
+            //x %= 2;
+            System.Console.WriteLine(x);
+
+            int z = 2 * ++x;
+            System.Console.WriteLine(x);
+            System.Console.WriteLine(z);
+        }
+    }
+}
