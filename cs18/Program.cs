29b242388bcb7df078c435c5eb2e059aaba4dc09
HuynhVanDung <vandung1307200@gmail.com>
2025-09-07 20:48:54 +0700

    [cs18] null,nullable

---
+++ cs18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace cs18
+{
+    class Abc
+    {
+        public void xinChao() => System.Console.WriteLine("Hello");
+    }
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //null, nullable
+            //null - khong tham chieu den gia tri nao
+            //nullable - gan gia tri null cho bien tham tri  
+
+            Abc a = new Abc();
+            if (a != null)
+                a.xinChao();
+            a?.xinChao();
+
+
+            //nullable
+            int? age;
+            age = null; // int age =null {Loi}
+            age = 10;
+            if (age != null)
+            {
+                int age_ = (int)age;
+                System.Console.WriteLine(age_);
+            }
+        }
+    }
+}
